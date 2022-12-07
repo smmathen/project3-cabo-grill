6beee0e540e2bebefe5176d3404cbf77fad6637d
Shawn <70829009+smmathen@users.noreply.github.com>
2022-12-07 17:29:36 -0600

    Add files via upload

--- pages/employees/[...params].js
+++ pages/employees/[...params].js
@@ -1,10 +1,18 @@
 import Server from "next/dist/server/base-server";
+import { useRouter } from "next/router";
 import Login from "../../components/Login";
 import Serverui from "../../components/Serverui";
 
 export default function employee({params}) {
     const [name, role] = params || [];
-    return role == 'guest' ? <Login /> : <Serverui />
+    if(role === 'guest') {
+        return <Login/>
+    } else if (role === 'Server') {
+        return <Serverui/>
+    } else if (role === 'Manager') {
+        const router = useRouter();
+        router.push("/manager");
+    }
 
 }
 
