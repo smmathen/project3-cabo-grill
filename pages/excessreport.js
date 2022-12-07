5438fd668e0d5a5c6aea693cbe44ae6d0b27aca3
Shawn <70829009+smmathen@users.noreply.github.com>
2022-12-07 15:47:01 -0600

    Add files via upload

--- pages/excessreport.js
+++ pages/excessreport.js
@@ -5,25 +5,27 @@ import Seo from "../components/Seo";
 
 export default function excessreport() {
     const [time, setTime] = useState();
-    const getExcessReport = async (e) => {
-        e.preventDefault();
+    const [items, setItems] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    const getExcessReport = async () => {
         try {
-            const response = await fetch(`http://localhost:3001/excessReport`, {
+            const response = await fetch("https://project3-backend.onrender.com/excessReport", {
                 method: "POST",
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({"time": time})
+                body: JSON.stringify({ "time": time })
             })
-            .then(response => response.json())
-            .then(response => {
-                if (response["success"]) {
-                    alert("yay");
-                } else {
-                    alert("wrong time frame!");
-                }
-            });
+                .then(response => response.json())
+                .then(response => {
+                    if (response["success"]) {
+                        setItems(response["items"]);
+                        setLoaded(true);
+                    } else {
+                        alert("wrong time frame!");
+                    }
+                });
         } catch (err) {
             console.log(err.message);
         }
@@ -31,16 +33,18 @@ export default function excessreport() {
     return (
         <>
             <ManBar />
-            
-            {" "}
-            <Seo />
-            
+            <Seo title="Excess Report" />
+
             <h1>Date : </h1>
             <input type={"date"} onChange={e => setTime(e.target.value)}></input>
             <div className='dates-submit-button'>
-                <button type='button' className='btn btn-warning' onClick={e => getExcessReport}>Get Report</button>
+                <button type='button' className='btn btn-warning' onClick={getExcessReport}>Get Report</button>
             </div>
             <div style={{ justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <h1>Excess items</h1>
+                <ul>
+                    {items.map((item) => loaded ? <h3>{item}</h3> : <h3></h3>)}
+                </ul>
             </div>
         </>
     );
